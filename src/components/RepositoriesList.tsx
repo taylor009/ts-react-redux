c9499b5b83a186b8290d8fa81957c4c7aa4db5fe
Taylor <taylor@baremetalcloud.com>
2021-04-29 04:20:57 -0400

    hooked up RepositoriesList.tsx to redux store

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -19,6 +19,10 @@ const RepositoriesList: React.FC = () => {
                 <input value={term} type="text" onChange={e => setTerm(e.target.value)}/>
                 <button>Search</button>
             </form>
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading...</h3>}
+            {!error && !loading &&
+            data.map((name) => <div key={name}>{name}</div>)}
         </div>
     );
 };
